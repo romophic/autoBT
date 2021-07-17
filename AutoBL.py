e40afec44f79a0a79f3123403ebb24b4d1764837
NULLCT <cockatiel.u10@gmail.com>
2021-07-17 19:17:21 +0900

    add maketimefunction

--- AutoBL.py
+++ AutoBL.py
@@ -11,34 +11,39 @@ LOGIN_PW = ""
 BODYTEMP_LOW = 36.5
 BODYTEMP_MAX = 36.7
 
-import random
 import time
+import random
+import datetime
 from selenium import webdriver
+
+def makeTime() -> str:
+  return str(datetime.datetime.now().strftime("%Y/%m/%d"))
 
 def makeBodyTemp() -> str:
   return str(int((random.randrange(int(BODYTEMP_LOW*10),int(BODYTEMP_MAX*10)+1,1)+random.randrange(int(BODYTEMP_LOW*10),int(BODYTEMP_MAX*10)+1,1))/2) / 10)
 
-rawurl = "https://docs.google.com/forms/d/e/1FAIpQLSegJ5_ypMJ10ZSQZbLH-Onkv-Ca2u0xLmJ_xNIRyfFCThM3Jw/viewform?usp=pp_url&entry.1061678014=2021/07/17&entry.1274018874="+makeBodyTemp()
-
+rawurl = "https://docs.google.com/forms/d/e/1FAIpQLSegJ5_ypMJ10ZSQZbLH-Onkv-Ca2u0xLmJ_xNIRyfFCThM3Jw/viewform?usp=pp_url&entry.1061678014=" + \
+  makeTime() + "&entry.1274018874=" + \
+  makeBodyTemp()
 
 if __name__ == "__main__":
   driver = webdriver.Chrome()
   driver.get(rawurl)
 
-  time.sleep(2)
+  time.sleep(4)
 
   driver.find_element_by_xpath("//*[@id='identifierId']").send_keys(LOGIN_ID)
   driver.find_element_by_xpath("//*[@id='identifierNext']").click()
 
-  time.sleep(2)
+  time.sleep(4)
 
   driver.find_element_by_xpath("//*[@id='password']/div[1]/div/div[1]/input").send_keys(LOGIN_PW)
   driver.find_element_by_xpath("//*[@id='passwordNext']").click()
 
-  time.sleep(2)
+  time.sleep(4)
 
   driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div').click()
 
-  time.sleep(2)
+  time.sleep(4)
 
   driver.close()
